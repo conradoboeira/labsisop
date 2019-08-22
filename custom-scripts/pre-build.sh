612c01477e7b9cb7e919fd1e636b777b3539e2df
Conrado <conrado.boeira@acad.pucrs.br>
2019-08-22 10:05:00 -0300

    Server funcionando

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -2,3 +2,8 @@
   
 cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
 chmod +x $BASE_DIR/target/etc/init.d/S41network-config
+cp $BASE_DIR/../custom-scripts/simple_http_server.py $BASE_DIR/target/usr/bin/
+chmod +x $BASE_DIR/target/usr/bin/simple_http_server.py
+cp $BASE_DIR/../custom-scripts/S42server-config $BASE_DIR/target/etc/init.d
+chmod +x $BASE_DIR/target/etc/init.d/S42server-config
+
